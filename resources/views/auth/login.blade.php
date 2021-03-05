1ab13a0193d881069a0e97df8dca0cf1b32dc5dc
Bun933Gum <vhanzickay@gmail.com>
2021-03-05 13:56:21 +0700

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,95 +1,101 @@
+@php
+$configData = Helper::applClasses();
+@endphp
 @extends('layouts/fullLayoutMaster')
 
 @section('title', 'Login Page')
 
 @section('page-style')
-{{-- Page Css files --}}
-<link rel="stylesheet" href="{{ asset(mix('css/base/pages/page-auth.css')) }}">
+  {{-- Page Css files --}}
+  <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('css/base/pages/page-auth.css')) }}">
 @endsection
 
 @section('content')
-<div class="auth-wrapper auth-v1 px-2">
-  <div class="auth-inner py-2">
-    <!-- Login v1 -->
-    <div class="card mb-0">
-      <div class="card-body">
-        <a href="javascript:void(0);" class="brand-logo">
-          <span class="brand-logo">            
-            <img src="{{asset('images/icons/jasuko-icon.svg')}}" alt="Jasuko logo" />
-          </span>
-          <h2 class="brand-text">Jasuko</h2>
-        </a>
-
-        <h4 class="card-title mb-1">Welcome to Vuexy! 👋</h4>
-        <p class="card-text mb-2">Please sign-in to your account and start the adventure</p>
-
-        <form class="auth-login-form mt-2" method="POST" action="{{ route('login') }}">
-          @csrf
-          <div class="form-group">
-            <label for="login-email" class="form-label">Email</label>
-            <input type="text" class="form-control @error('email') is-invalid @enderror" id="login-email" name="email" placeholder="john@example.com" aria-describedby="login-email" tabindex="1" autofocus value="{{ old('email') }}" />
-            @error('email')
-              <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-              </span>
-            @enderror
-          </div>
-
-          <div class="form-group">
-            <div class="d-flex justify-content-between">
-              <label for="login-password">Password</label>
-              @if (Route::has('password.request'))
-              <a href="{{ route('password.request') }}">
-                <small>Forgot Password?</small>
-              </a>
-              @endif
-            </div>
-            <div class="input-group input-group-merge form-password-toggle">
-              <input type="password" class="form-control form-control-merge" id="login-password" name="password" tabindex="2" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="login-password" />
-              <div class="input-group-append">
-                <span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span>
-              </div>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="custom-control custom-checkbox">
-              <input class="custom-control-input" type="checkbox" id="remember-me" name="remember-me" tabindex="3" {{ old('remember-me') ? 'checked' : '' }} />
-              <label class="custom-control-label" for="remember-me"> Remember Me </label>
-            </div>
-          </div>
-          <button type="submit" class="btn btn-primary btn-block" tabindex="4">Sign in</button>
-        </form>
-
-        <p class="text-center mt-2">
-          <span>New on our platform?</span>
-          @if (Route::has('register'))
-          <a href="{{ route('register') }}">
-            <span>Create an account</span>
-          </a>
+<div class="auth-wrapper auth-v2">
+  <div class="auth-inner row m-0">
+      <!-- Brand logo-->
+      <a class="brand-logo" href="javascript:void(0);">
+        <h2 class="brand-text">Jasuko</h2>
+      </a>
+      <!-- /Brand logo-->
+      <!-- Left Text-->
+      <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
+        <div class="w-100 d-lg-flex align-items-center justify-content-center px-5">
+          @if($configData['theme'] === 'dark')
+          <img class="img-fluid" src="{{asset('images/pages/login-v2-dark.svg')}}" alt="Login V2" />
+          @else
+          <img class="img-fluid" src="{{asset('images/pages/login-v2.svg')}}" alt="Login V2" />
           @endif
-        </p>
-
-        <div class="divider my-2">
-          <div class="divider-text">or</div>
-        </div>
-
-        <div class="auth-footer-btn d-flex justify-content-center">
-          <a href="javascript:void(0)" class="btn btn-facebook">
-            <i data-feather="facebook"></i>
-          </a>
-          <a href="javascript:void(0)" class="btn btn-twitter white">
-            <i data-feather="twitter"></i>
-          </a>
-          <a href="javascript:void(0)" class="btn btn-google">
-            <i data-feather="mail"></i>
-          </a>
-          <a href="javascript:void(0)" class="btn btn-github">
-            <i data-feather="github"></i>
-          </a>
         </div>
       </div>
+      <!-- /Left Text-->
+      <!-- Login-->
+      <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
+        <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
+          <h2 class="card-title font-weight-bold mb-1">Welcome to Vuexy! &#x1F44B;</h2>
+          <p class="card-text mb-2">Please sign-in to your account and start the adventure</p>
+          <form class="auth-login-form mt-2" action="/" method="GET">
+            <div class="form-group">
+              <label class="form-label" for="login-email">Email</label>
+              <input class="form-control" id="login-email" type="text" name="login-email" placeholder="john@example.com" aria-describedby="login-email" autofocus="" tabindex="1" />
+            </div>
+            <div class="form-group">
+              <div class="d-flex justify-content-between">
+                <label for="login-password">Password</label>
+                <a href="{{url("auth/forgot-password-v2")}}">
+                  <small>Forgot Password?</small>
+                </a>
+              </div>
+              <div class="input-group input-group-merge form-password-toggle">
+                <input class="form-control form-control-merge" id="login-password" type="password" name="login-password" placeholder="············" aria-describedby="login-password" tabindex="2" />
+                <div class="input-group-append">
+                  <span class="input-group-text cursor-pointer">
+                    <i data-feather="eye"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <div div class="custom-control custom-checkbox">
+                <input class="custom-control-input" id="remember-me" type="checkbox" tabindex="3" />
+                <label class="custom-control-label" for="remember-me">Remember Me</label>
+              </div>
+            </div>
+            <button class="btn btn-primary btn-block" tabindex="4">Sign in</button>
+          </form>
+          <p class="text-center mt-2">
+            <span>New on our platform?</span>
+            <a href="{{url('auth/register-v2')}}"><span>&nbsp;Create an account</span></a>
+          </p>
+          <div class="divider my-2">
+            <div class="divider-text">or</div>
+          </div>
+          <div class="auth-footer-btn d-flex justify-content-center">
+            <a class="btn btn-facebook" href="javascript:void(0)">
+              <i data-feather="facebook"></i>
+            </a>
+            <a class="btn btn-twitter white" href="javascript:void(0)">
+              <i data-feather="twitter"></i>
+            </a>
+            <a class="btn btn-google" href="javascript:void(0)">
+              <i data-feather="mail"></i>
+            </a>
+            <a class="btn btn-github" href="javascript:void(0)">
+              <i data-feather="github"></i>
+            </a>
+          </div>
+      </div>
     </div>
-    <!-- /Login v1 -->
+    <!-- /Login-->
   </div>
 </div>
+@endsection
+
+@section('vendor-script')
+<script src="{{asset(mix('vendors/js/forms/validation/jquery.validate.min.js'))}}"></script>
+@endsection
+
+@section('page-script')
+<script src="{{asset(mix('js/scripts/pages/page-auth-login.js'))}}"></script>
 @endsection
